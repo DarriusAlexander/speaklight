525262c6a5129ee7fac04e3c0a18d71115efd291
DarriusAlexander <darriush@mail.usf.edu>
2020-02-02 16:53:22 -0500

    .

--- rootfs/download-extra.sh
+++ rootfs/download-extra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -LO https://storage.cloud.google.com/wp-speaklight/wp-content.zip?cloudshell=true&orgonly=true&supportedpurview=organizationId | grep -oP '"tag_name": "\K(.*)(?=")'
+curl https://storage.cloud.google.com/wp-speaklight/wp-content.zip?cloudshell=true&orgonly=true&supportedpurview=organizationId | grep -oP '"tag_name": "\K(.*)(?=")'
 unzip wp-content.zip -d /opt/bitnami/wordpress
 
 
