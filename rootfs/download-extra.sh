db629bd93fbedbdbff676e1c66260d08f0453b89
DarriusAlexander <darriush@mail.usf.edu>
2020-02-02 04:31:03 -0500

    respec 2

--- rootfs/download-extra.sh
+++ rootfs/download-extra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -o https://github.com/DarriusAlexander/speaklight-content/archive/master.zip
-unzip /tmp/master.zip -d /opt/bitnami/wordpress/wp-content
+unzip -d /opt/bitnami/wordpress/wp-content
 
 
 
