ae73412ae1bd5af571a900f7ee278b9949e58d4a
DarriusAlexander <darriush@mail.usf.edu>
2020-02-04 00:47:42 -0500

    .

--- rootfs/download-extra.sh
+++ rootfs/download-extra.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -LO https://github.com/DarriusAlexander/speaklight-content/archive/master.zip  | grep -oP '"tag_name": "\K(.*)(?=")'
-unzip master.zip -d /opt/bitnami/wordpress
-mv /opt/bitnami/wordpress/wp-content-master /opt/bitnami/wp-content
+unzip master.zip -d /bitnami/wordpress
+mv /bitnami/wordpress/wp-content-master /bitnami/wordpress/wp-content
